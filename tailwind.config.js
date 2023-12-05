aab0c718fc5371ce69d9d35f048c71b633a72e5c
Fer Moreno <fernandomoreno@outlook.com>
2023-12-04 21:34:27 -0600

    tailwinds baby

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const { join } = require('path');
+
+module.exports = {
+  content: [
+    "./src/**/*.{html, js, ts, vue}", 
+    "./src/**/*",
+    "index.html"
+
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
